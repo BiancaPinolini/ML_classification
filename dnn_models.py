813e4c76a3fe22ba7a4f8e26573b2995729797c7
Bianca Pinolini <b.pinolini@tim.it>
2020-11-27 12:26:32 +0100

    Latest updates

--- dnn_models.py
+++ dnn_models.py
@@ -1,16 +1,50 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, BatchNormalization, Dropout
-from keras import optimizers
-from keras import regularizers
+from keras import regularizers, optimizers
 
 def get_model(model_tag, input_dim):
 
     print(">>> Creating model...")
     model = Sequential()
     
+    if model_tag == "3l_150n_l2_batchnorm_relu":
+        model.add(Dense(150, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dense(150,activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dense(150,activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dense(1, activation="sigmoid"))
+        return model
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if model_tag == "2l_10n_relu":
+        model.add(Dense(10, input_dim=input_dim, activation="relu"))
+        model.add(Dense(10, activation="relu"))        
+        model.add(Dense(1, activation="sigmoid"))
+        return model
+
     if model_tag == "2l_50n_relu":
         model.add(Dense(50, input_dim=input_dim, activation="relu"))
         model.add(Dense(50, activation="relu"))        
+        model.add(Dense(1, activation="sigmoid"))
+        return model
+
+    if model_tag == "2l_50n_batchnorm_relu":
+        model.add(Dense(50, input_dim=input_dim, activation="relu"))
+        model.add(BatchNormalization())
+        
+        model.add(Dense(50, activation="relu"))    
+        model.add(BatchNormalization())   
+        
         model.add(Dense(1, activation="sigmoid"))
         return model
     
@@ -48,28 +82,31 @@ def get_model(model_tag, input_dim):
         model.add(Dense(1, activation="sigmoid"))
         return model
     
-    if model_tag == "3l_100n_relu":
-        model.add(Dense(100, input_dim=input_dim, activation="relu"))
+    if model_tag == "3l_100n_l2_batchnorm_relu":
+        model.add(Dense(100, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
         model.add(Dense(100, activation="relu"))
-        model.add(Dense(100, activation="relu"))        
+        model.add(BatchNormalization())
+        model.add(Dense(100, activation="relu"))    
+        model.add(BatchNormalization())    
         model.add(Dense(1, activation="sigmoid"))
         return model
     
-    if model_tag == "3l_100n_dropout01_relu":
+    if model_tag == "3l_100n_dropout005_relu":
         model.add(Dense(100, input_dim=input_dim, activation="relu"))
-        model.add(Dropout(0.1))
+        model.add(Dropout(0.05))
         
         model.add(Dense(100, activation="relu"))
-        model.add(Dropout(0.1))
+        model.add(Dropout(0.05))
         
         model.add(Dense(100, activation="relu"))   
-        model.add(Dropout(0.1))     
+        model.add(Dropout(0.05))     
         
         model.add(Dense(1, activation="sigmoid"))
         
         return model
 
-    if model_tag == "3l_50n_dropout01_l2_batchnorm_relu":
+    if model_tag == "3l_50n_dropout05_l2_batchnorm_relu":
         model.add(Dense(50, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())
         model.add(Dropout(0.5))
@@ -101,22 +138,22 @@ def get_model(model_tag, input_dim):
         model.add(Dense(1, activation="sigmoid"))
         return model
     
-    if model_tag == "4l_50n_dropout05_l2_batchnorm_relu":
+    if model_tag == "4l_50n_dropout01_l2_batchnorm_relu":
         model.add(Dense(50, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.1))
         
         model.add(Dense(50, activation="relu",kernel_regularizer=regularizers.l2(0.01)))    
         model.add(BatchNormalization())   
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.1))
         
         model.add(Dense(50, activation="relu"))    
         model.add(BatchNormalization())   
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.1))
         
         model.add(Dense(50, activation="relu"))    
         model.add(BatchNormalization())   
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.1))
         
         model.add(Dense(1, activation="sigmoid"))
         return model
@@ -138,22 +175,22 @@ def get_model(model_tag, input_dim):
         
         return model
     
-    if model_tag == "4l_100n_dropout05_l2_batchnorm_relu":
+    if model_tag == "4l_100n_dropout02_l2_batchnorm_relu":
         model.add(Dense(150, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())   
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.2))
         
         model.add(Dense(100,activation="relu",kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())   
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.2))
         
         model.add(Dense(100,activation="relu"))
         model.add(BatchNormalization())   
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.2))
         
         model.add(Dense(100,activation="relu"))
         model.add(BatchNormalization())   
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.2))
         
         model.add(Dense(1, activation="sigmoid"))
         
@@ -216,6 +253,30 @@ def get_model(model_tag, input_dim):
         model.add(Dropout(0.5))
         
         model.add(Dense(1, activation="sigmoid"))
+        return model
+    
+    if model_tag == "4l_100n_l2_batchnorm_relu":
+        model.add(Dense(100, input_dim=input_dim, activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.01))
+               
+        model.add(Dense(100, kernel_regularizer=regularizers.l2(0.01)))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.01))
+        
+        model.add(Dense(100))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.01))
+        
+        model.add(Dense(100))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.01))
+                  
+        model.add(Dense(1, activation="sigmoid"))
+        
         return model
     
     if model_tag == "5l_50n_dropout01_relu":
@@ -313,6 +374,35 @@ def get_model(model_tag, input_dim):
         
         return model
     
+    if model_tag == "5l_200n_l2_batchnorm_relu":
+        model.add(Dense(200, input_dim=input_dim, activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.01))
+               
+        model.add(Dense(200, kernel_regularizer=regularizers.l2(0.01)))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.01))
+        
+        model.add(Dense(200))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.01))
+        
+        model.add(Dense(200))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.01))
+        
+        model.add(Dense(200))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.01))
+        
+        model.add(Dense(1, activation="sigmoid"))
+        
+        return model
+    
     if model_tag == "5l_100n_dropout05_l2_batchnorm_relu":
         model.add(Dense(150, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())
@@ -335,6 +425,118 @@ def get_model(model_tag, input_dim):
         
         return model
     
+    if model_tag == "5l_200n_dropout05_l2_batchnorm_relu":
+        model.add(Dense(200, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        
+        model.add(Dense(200,activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        
+        model.add(Dense(200,activation="relu"))
+        model.add(Dropout(0.4))
+        
+        model.add(Dense(200,activation="relu"))
+        model.add(Dropout(0.4))
+        
+        model.add(Dense(200,activation="relu"))
+        model.add(Dropout(0.4))
+        
+        model.add(Dense(1, activation="sigmoid"))
+        
+        return model
+    
+    if model_tag == "5l_150n_dropout02_l2_batchnorm_relu":
+        model.add(Dense(200, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.7))
+        
+        model.add(Dense(200,activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.7))
+        
+        model.add(Dense(150,activation="relu"))
+        model.add(Dropout(0.2))
+        
+        model.add(Dense(150,activation="relu"))
+        model.add(Dropout(0.2))
+        
+        model.add(Dense(150,activation="relu"))
+        model.add(Dropout(0.2))
+        
+        model.add(Dense(1, activation="sigmoid"))
+        
+        return model
+    
+    if model_tag == "5l_150n_dropout02_l2_batchnorm_relu":
+        model.add(Dense(200, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.7))
+        
+        model.add(Dense(200,activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.7))
+        
+        model.add(Dense(150,activation="relu"))
+        model.add(Dropout(0.2))
+        
+        model.add(Dense(150,activation="relu"))
+        model.add(Dropout(0.2))
+        
+        model.add(Dense(150,activation="relu"))
+        model.add(Dropout(0.2))
+        
+        model.add(Dense(1, activation="sigmoid"))
+        
+        return model
+    
+    if model_tag == "5l_300n_dropout02_l2_batchnorm_relu":
+        model.add(Dense(300, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.7))
+        
+        model.add(Dense(300,activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.7))
+        
+        model.add(Dense(200,activation="relu"))
+        model.add(Dropout(0.4))
+        
+        model.add(Dense(200,activation="relu"))
+        model.add(Dropout(0.4))
+        
+        model.add(Dense(200,activation="relu"))
+        model.add(Dropout(0.4))
+        
+        model.add(Dense(1, activation="sigmoid"))
+        
+        return model
+   
+    if model_tag == "6l_70n_dropout05_l2_batchnorm_relu":
+        model.add(Dense(100, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        
+        model.add(Dense(100,activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        
+        model.add(Dense(80,activation="relu", kernel_regularizer=regularizers.l2(0.01)))
+        model.add(Dropout(0.5))
+        
+        model.add(Dense(70,activation="relu"))
+        model.add(Dropout(0.5))
+        
+        model.add(Dense(70,activation="relu"))
+        model.add(Dropout(0.5))
+        
+        model.add(Dense(70,activation="relu"))
+        model.add(Dropout(0.5))
+        
+        model.add(Dense(1, activation="sigmoid"))
+        
+        return model
     
     if model_tag == "6l_100n_dropout05_l2_batchnorm_relu":
         model.add(Dense(150, input_dim=input_dim, activation="relu",kernel_regularizer=regularizers.l2(0.01)))
@@ -361,60 +563,157 @@ def get_model(model_tag, input_dim):
         
         return model
     
+    if model_tag == "6l_256n_l2_batchnorm_tanh":
+        model.add(Dense(256, input_dim=input_dim,kernel_regularizer=regularizers.l2(0.01)))
+        model.add(Activation('tanh'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(256,kernel_regularizer=regularizers.l2(0.01)))
+        model.add(Activation('tanh'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(128,kernel_regularizer=regularizers.l2(0.01)))
+        model.add(Activation('tanh'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(64))
+        model.add(Activation('tanh'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(64))
+        model.add(Activation('tanh'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(64))
+        model.add(Activation('tanh'))
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(1, activation="sigmoid"))
+        return model
     
-    if model_tag == "6l_64n_l2_batchnorm_tanh":
+    if model_tag == "6l_256n_l2_batchnorm_relu":
+        model.add(Dense(256, input_dim=input_dim,kernel_regularizer=regularizers.l2(0.01)))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(256,kernel_regularizer=regularizers.l2(0.01)))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(128,kernel_regularizer=regularizers.l2(0.01)))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(64))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.3))
+        
+        model.add(Dense(1, activation="sigmoid"))
+        return model
+    
+    if model_tag == "8l_64n_dropout01_l2_batchnorm_relu":
         model.add(Dense(64, input_dim=input_dim,kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())
-        model.add(Activation("tanh")) 
+        model.add(Activation("relu")) 
+        model.add(Dropout(0.3))
         
         model.add(Dense(64,kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())
-        model.add(Activation("tanh")) 
+        model.add(Activation("relu")) 
+        model.add(Dropout(0.3))
         
         model.add(Dense(64,kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())
-        model.add(Activation("tanh")) 
+        model.add(Activation("relu")) 
+        model.add(Dropout(0.3))
         
         model.add(Dense(32))
         model.add(BatchNormalization())
-        model.add(Activation("tanh")) 
+        model.add(Activation("relu")) 
+        model.add(Dropout(0.1))
         
         model.add(Dense(32))
         model.add(BatchNormalization())
-        model.add(Activation("tanh")) 
+        model.add(Activation("relu")) 
+        model.add(Dropout(0.1))
         
         model.add(Dense(32))
         model.add(BatchNormalization())
-        model.add(Activation("tanh")) 
+        model.add(Activation("relu")) 
+        model.add(Dropout(0.1))
+        
+        model.add(Dense(32))
+        model.add(BatchNormalization())
+        model.add(Activation("relu")) 
+        model.add(Dropout(0.1))
+        
+        model.add(Dense(32))
+        model.add(BatchNormalization())
+        model.add(Activation("relu"))
+        model.add(Dropout(0.1)) 
         
         model.add(Dense(1, activation="sigmoid"))
         
         return model
     
-    if model_tag == "6l_64n_l2_batchnorm_relu":
+    if model_tag == "8l_64n_dropout005_l2_batchnorm_relu":
         model.add(Dense(64, input_dim=input_dim,kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())
         model.add(Activation("relu")) 
+        model.add(Dropout(0.2))
         
         model.add(Dense(64,kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())
         model.add(Activation("relu")) 
+        model.add(Dropout(0.2))
         
         model.add(Dense(64,kernel_regularizer=regularizers.l2(0.01)))
         model.add(BatchNormalization())
         model.add(Activation("relu")) 
+        model.add(Dropout(0.2))
         
         model.add(Dense(32))
         model.add(BatchNormalization())
         model.add(Activation("relu")) 
+        model.add(Dropout(0.1))
         
         model.add(Dense(32))
         model.add(BatchNormalization())
         model.add(Activation("relu")) 
+        model.add(Dropout(0.05))
         
         model.add(Dense(32))
         model.add(BatchNormalization())
         model.add(Activation("relu")) 
+        model.add(Dropout(0.05))
+        
+        model.add(Dense(32))
+        model.add(BatchNormalization())
+        model.add(Activation("relu")) 
+        model.add(Dropout(0.05))
+        
+        model.add(Dense(32))
+        model.add(BatchNormalization())
+        model.add(Activation("relu"))
+        model.add(Dropout(0.05)) 
         
         model.add(Dense(1, activation="sigmoid"))
         
--- dnn_plot_loss.py
+++ dnn_plot_loss.py
@@ -39,8 +39,6 @@ class PlotLosses(keras.callbacks.Callback):
         self.fig = plt.figure()
         self.dnn_score_plot = []
         self.dnn_score_log = []
-        self.kstest_sig = []
-        self.kstest_bkg = []
         self.significance_test = []
         self.significance_train = []
         self.precision_test = []
@@ -73,39 +71,6 @@ class PlotLosses(keras.callbacks.Callback):
         self.pred_train_temp = self.model.predict(self.X_train, batch_size=4096)
         self.pred_test_temp = np.array(self.pred_test_temp).flatten()
         self.pred_train_temp = np.array(self.pred_train_temp).flatten()
-        
-        # auc_w_test = roc_auc_score(self.y_test, self.pred_test_temp, sample_weight=self.W_test)
-        # self.auc_test.append(auc_w_test)
-        
-        # auc_w_train = roc_auc_score(self.y_train, self.pred_train_temp, sample_weight=self.W_train)
-        # self.auc_train.append(auc_w_train)
-
-        kstest_pval_sig = stats.ks_2samp(self.pred_train_temp[self.y_train==1], self.pred_test_temp[self.y_test==1], mode="asymp") # (statistics, pvalue)
-        self.kstest_sig.append(kstest_pval_sig[1])
-        kstest_pval_bkg = stats.ks_2samp(self.pred_train_temp[self.y_train==0], self.pred_test_temp[self.y_test==0], mode="asymp") # (statistics, pvalue)
-        self.kstest_bkg.append(kstest_pval_bkg[1])
-        # print("KS test (dnn output: sig (train) vs sig (val))", kstest_pval_sig, ". good: ", kstest_pval_sig[1] > 0.05)
-        # print("KS test (dnn output: bkg (train) vs bkg (val))", kstest_pval_bkg, ". good: ", kstest_pval_bkg[1] > 0.05)
-
-        #print(self.y_train.shape, self.y_train[self.y_train==1].shape, self.y_train[self.y_train==0].shape,)
-        #print(self.X_train.shape, )
-        # s_great_train_mask = (self.y_train==1) & (pred_train[self.y_train==1] > 0.8)
-        # print("train", self.X_train.shape, self.y_train.shape, self.W_train.shape, self.pred_train_temp.shape )
-        #print("pred", pred_train[self.y_train==1].shape, len(pred_train[self.y_train==1]) )
-        #print("W", self.W_train[self.y_train==1].shape)
-        #s_tot = np.zeros(len(pred_train))
-
-        TP_train = self.Wnn_train[(self.y_train==1) & (self.pred_train_temp > self.dnncut)].sum()
-        FP_train = self.Wnn_train[(self.y_train==0) & (self.pred_train_temp > self.dnncut)].sum() 
-        T_train = self.Wnn_train[(self.y_train==1)].sum()
-        significance_train =  TP_train  / (np.sqrt( FP_train ))
-        self.significance_train.append(significance_train)
-
-        TP_test = self.Wnn_test[(self.y_test==1) & (self.pred_test_temp > self.dnncut)].sum()
-        FP_test = self.Wnn_test[(self.y_test==0) & (self.pred_test_temp > self.dnncut)].sum()
-        T_test = self.Wnn_test[(self.y_test==1)].sum()
-        significance_test =  TP_test  / (np.sqrt( FP_test ))
-        self.significance_test.append(significance_test)
 
         self.precision_test.append(TP_test/ (TP_test+FP_test))
         self.precision_train.append(TP_train/ (TP_train+FP_train))
@@ -113,7 +78,7 @@ class PlotLosses(keras.callbacks.Callback):
         self.recall_train.append(TP_train/ T_train)
 
     def performance_plot(self):
-        self.figure, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(24,10))
+        self.figure, ((ax1, ax2, ax3), (ax4, , ax6)) = plt.subplots(2, 3, figsize=(24,10))
 
         # ax1.set_yscale('log')
         ax1.plot(self.x, self.losses, "o-", label="loss (train)")
@@ -137,26 +102,13 @@ class PlotLosses(keras.callbacks.Callback):
         ax4.hist(self.pred_train_temp[self.y_train==1],weights=self.W_train[self.y_train==1], bins=bins, range=(0.,1.), density=True, label="sig (train)", histtype="step")
         dnnout_false = ax4.hist(self.pred_test_temp[self.y_test==0],weights=self.W_test[self.y_test==0], bins=bins, range=(0.,1.), density=True, label="bkg (val)", histtype="step")
         dnnout_true  = ax4.hist(self.pred_test_temp[self.y_test==1],weights=self.W_test[self.y_test==1], bins=bins, range=(0.,1.), density=True, label="sig (val)", histtype="step")
-        ax4.set_xlabel("DNN output")
+        ax4.set_xlabel("DNN score")
         ax4.legend()
-
-        ax5.plot(self.x, self.kstest_sig, "o-", label="sig (train) vs sig (val). kstest pval")
-        ax5.plot(self.x, self.kstest_bkg, "o-", label="bkg (train) vs bkg (val). kstest pval")
-        ax5.plot((self.x[0], self.x[-1]), (0.05, 0.05), 'k-')
-        ax5.legend()
-        ax5.set_xlabel("epochs")
-        ax5.set_yscale('log')
 
         ax6.plot(self.x, self.recall_train, "o-", label="Recall (train) thr0.85")
         ax6.plot(self.x, self.recall_test, "o-", label="Recall (test) thr0.85")
         ax6.set_xlabel("epochs")
         ax6.legend()
-        # ax6.plot(self.x, self.significance_train, "o-", color="blue")
-        # ax6.set_ylabel("S / sqrt(B) (train)", color='blue')
-        # ax6.set_xlabel("epochs")
-        # ax7 = ax6.twinx()
-        # ax7.plot(self.x, self.significance_test, "o-", color="orange")
-        # ax7.set_ylabel("S / sqrt(B) (val)", color='orange')
 
         if not self.batch_mode:
             clear_output(wait=True)
