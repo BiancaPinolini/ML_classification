f157c0df8a9e2465bf5a076d40a2eea9b46c22f3
Bianca Pinolini <b.pinolini@tim.it>
2021-01-27 12:55:07 +0100

    Bug corrections

--- dnn_models.py
+++ dnn_models.py
@@ -30,16 +30,13 @@ def get_model(model_tag, input_dim):
         model.add(Dense(1, activation="sigmoid"))
         return model
     
-    if model_tag == "5l_64n_l2_batchnorm_relu":
-        model.add(Dense(64, input_dim=input_dim, kernel_regularizer=regularizers.l2(0.01)))
+    if model_tag == "2l_32n_l2_batchnorm_relu":
+        model.add(Dense(32, input_dim=input_dim, kernel_regularizer=regularizers.l2(0.03)))
         model.add(BatchNormalization())
         model.add(Activation("relu"))
         
-        model.add(Dense(32, input_dim=input_dim, kernel_regularizer=regularizers.l2(0.01)))
+        model.add(Dense(32, kernel_regularizer=regularizers.l2(0.03)))
         model.add(BatchNormalization())
-        model.add(Activation("relu"))
-        
-        model.add(Dense(32, input_dim=input_dim, kernel_regularizer=regularizers.l2(0.01)))
         model.add(Activation("relu"))
         
         model.add(Dense(1, activation="sigmoid"))
